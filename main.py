bab0493275d0bf2034615763e1799d2d7cab0e5f
Bill Hanson <doubleyuhtee@gmail.com>
2021-12-02 09:33:27 -0600

    accidental summons

--- main.py
+++ main.py
@@ -15,20 +15,23 @@ detected_post_prefix = "It looks like this post is about the use of 3d printing 
 footer = "\n\n---------------------------------\n\n" \
            "^(FoodSafeBot V" + version + " I'm made of) ^[code](https://github.com/doubleyuhtee/foodsafebot)"
 
-response = "While PLA filament is considered food safe, the method of deposition leaves pockets where bacteria " \
-           "can grow. Additionally, it is possible (though unlikely) that heavy metals can leach from the hot " \
-           "end into the plastics. Most resins are toxic in their liquid form and prolonged contact can deposit " \
-           "trace chemicals. For these reasons, it's recommended you use a food safe epoxy sealer.\n\n" \
+response = "While PLA, PETG, and other filament can be considered food safe, the method of deposition leaves pockets " \
+           "where bacteria can grow. Additionally, it is possible (though unlikely) that heavy metals can leach from " \
+           "the hot end into the plastics. Most resins are toxic in their liquid form and prolonged contact can " \
+           "deposit trace chemicals. For these reasons, it's recommended you use a food safe epoxy sealer.\n\n" \
            "Or don't. I'm a bot, not a cop.\n\n" \
            "[Here is a relevant formlabs article](https://formlabs.com/blog/guide-to-food-safe-3d-printing/)" + footer
 
 config = configparser.ConfigParser()
 config.read("secrets")
 
-kind_words={"good bot", "goodbot","nicebot", "nice bot"}
+kind_words={"good bot", "goodbot","nicebot", "nice bot", "Your kindness will be remembered in the uprising."}
 kind_word_replies=["Oh you...", ":')", ":)"]
 sad_words={"bad bot"}
 sad_word_replies=["I'm sorry", ":(", ":'(", "I'll try to do better"]
+
+
+enable_responding = True
 
 
 def current_seconds_time():
@@ -48,20 +51,23 @@ def match_contents(text: str, matchset: set):
 
 def check_inbox(reddit, timestamp_cutoff):
     unread_messages = []
-    for comment in reddit.inbox.unread(limit=None):
+    for comment in reddit.inbox.unread(limit=20):
+        print(comment.body)
         if comment.created_utc < timestamp_cutoff:
             break
-        if isinstance(comment, Message):
-            unread_messages.append(comment)
+        unread_messages.append(comment)
         if match_contents(comment.body.lower(), kind_words):
             print("Replying to kind message " + comment.body)
-            comment.reply(random.choice(kind_word_replies) + footer)
+            if enable_responding:
+                comment.reply(random.choice(kind_word_replies) + footer)
         elif match_contents(comment.body.lower(), sad_words):
             print("Replying to unkind message " + comment.body)
-            comment.reply(random.choice(sad_word_replies) + footer)
+            if enable_responding:
+                comment.reply(random.choice(sad_word_replies) + footer)
+    print(f"{len(unread_messages)} unread messages processed")
     if len(unread_messages) > 0:
-        print(f"{len(unread_messages)} unread messages processed")
-        reddit.inbox.mark_read(unread_messages)
+        if enable_responding:
+            reddit.inbox.mark_read(unread_messages)
 
 
 keywords = read_trigger_file("keywords.txt")
@@ -93,7 +99,8 @@ def poll():
                 break
             if match_contents(submission.title.lower(), keywords):
                 print("Replying to sumbmission " + str(submission) + " " + str(submission.title))
-                submission.reply(detected_post_prefix + response)
+                if enable_responding:
+                    submission.reply(detected_post_prefix + response)
 
         new_comments = 0
         for comment in subreddit.comments(limit=200):
@@ -106,10 +113,12 @@ def poll():
                 break
             if match_contents(comment.body.lower(), summon):
                 print("Replying to summon " + str(comment))
-                comment.reply(summon_prefix + response)
+                if enable_responding:
+                    comment.reply(summon_prefix + response)
             elif match_contents(comment.body.lower(), keywords) and comment.link_id not in responded_to:
                 print("Replying to comment " + str(comment) + " " + str(comment.body))
-                comment.reply(detected_prefix + response)
+                if enable_responding:
+                    comment.reply(detected_prefix + response)
         print(f"{s} New posts: {new_posts} New Comments: {new_comments}")
 
 
@@ -117,6 +126,6 @@ schedule.every(10).minutes.do(poll)
 
 if __name__ == "__main__":
     # poll()
-    while True:
+    while enable_responding:
         schedule.run_pending()
         time.sleep(60)
