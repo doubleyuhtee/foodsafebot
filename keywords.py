c5866239fce2ff789282783c015f6e91093310c1
Bill Hanson <doubleyuhtee@gmail.com>
2022-02-12 13:30:58 -0600

    more cookie cutter stuff

--- keywords.py
+++ keywords.py
@@ -3,7 +3,8 @@ from textmatcher.textmatch import *
 shpiel_keywords = Trigger(
     And(
         Not(Or("seal", "sealer", "ceramic", "formlabs.com/blog/")),
-        Or("cookie cutter", "foodsafe", "foodsafety", And("food", Or("safe", "safety")))
+        Or("cookie cutter", "cookiecutters", "cookiecutter", "cookie cutters",
+           "foodsafe", "foodsafety", And("food", Or("safe", "safety")))
     ))
 
 summon_keywords = Trigger(
